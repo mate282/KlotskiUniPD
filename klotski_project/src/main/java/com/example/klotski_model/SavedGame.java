753a12cc7d00d188846bc90d772338c69cab1cd2
mate282 <matteos2802@gmail.com>
2023-05-24 11:13:51 +0200

    Aggiunti metodi JSON a SavedGame e Level Solution

--- klotski_project/src/main/java/com/example/klotski_model/SavedGame.java
+++ klotski_project/src/main/java/com/example/klotski_model/SavedGame.java
@@ -1,7 +1,14 @@
 package com.example.klotski_model;
+import org.json.JSONObject;
 
-import java.io.Serializable;
+public class SavedGame {
+    public JSONObject toJSON(){
+        JSONObject savedJSON = new JSONObject();
+        return savedJSON;
+    }
 
-public class SavedGame implements Serializable {
-
+    static public SavedGame fromJSON(JSONObject jsonObject)
+    {
+        return new SavedGame();
+    }
 }
