ce7a4366b4f6f1684b028edab0b1550ca0fcf8e4
mate282 <matteos2802@gmail.com>
2023-05-29 12:29:36 +0200

    Test PersistenceDataService

--- klotski_project/src/main/java/com/klotski/model/PersistenceDataService.java
+++ klotski_project/src/main/java/com/klotski/model/PersistenceDataService.java
@@ -1,11 +1,14 @@
 package com.klotski.model;
 
 import com.klotski.klotski_project.KlotskiApp;
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
@@ -14,18 +17,16 @@ import java.util.Scanner;
 
 public class PersistenceDataService {
 
-    private static final String FILES_BASEPATH = "C:/GameData/";
-    private static final String SAVEFILE_PATH = "Saves/";
-    private static final String CONFIGFILE_PATH = "Configurations/";
-    private static final String SOLFILE_PATH = "Solutions/";
+    public static final String FILES_BASEPATH = "KlotskiGame/";
+    public static final String SAVEFILE_PATH = "Saves/";
+    public static final String CONFIGFILE_PATH = "Configurations/";
+    public static final String SOLFILE_PATH = "Solutions/";
 
-    private static final String SAVEFILE_NAME = "saving_";
-    private static final String CONFIGFILE_NAME = "saving_";
+    public static final String SAVEFILE_NAME = "saving_";
+    public static final String LISTFILE_NAME = "levelList";
+    public static final String FILE_EXT = ".json";
 
-    private static final String FILE_EXT = ".json";
-
-
-    private static final String DATE_FORMAT = "yyyyMMdd_HHmmss";
+    public static final String DATE_FORMAT = "yyyyMMdd_HHmmss";
 
     /**
      * Save game data to persistent location.
@@ -35,10 +36,14 @@ public class PersistenceDataService {
     public static boolean saveGameData(SavedGame gameToSave) {
 
         try{
+
+            String path = System.getProperty("user.home")+File.separator;
+            Files.createDirectories(Path.of(path+FILES_BASEPATH+SAVEFILE_PATH));
+
             //generate file name for saving : filePath+baseName+saveDate
             DateTimeFormatter dtf = DateTimeFormatter.ofPattern(DATE_FORMAT);
             LocalDateTime now = LocalDateTime.now();
-            String fileName = FILES_BASEPATH + SAVEFILE_PATH+SAVEFILE_NAME +dtf.format(now)+FILE_EXT;
+            String fileName = path + FILES_BASEPATH + SAVEFILE_PATH+SAVEFILE_NAME +dtf.format(gameToSave.getSaveDate())+FILE_EXT;
 
             //Create file
             File file = new File(fileName);
@@ -67,10 +72,11 @@ public class PersistenceDataService {
      * **/
     public static List<String> loadAllGameData(){
         ArrayList<String> savings = new ArrayList<String> (0);
+        String path = System.getProperty("user.home")+File.separator;
 
         try{
             //load all file in default directory
-            File dirFile = new File(FILES_BASEPATH+SAVEFILE_PATH);
+            File dirFile = new File(path+FILES_BASEPATH + SAVEFILE_PATH );
             //load all files in default directory
             File[] files = dirFile.listFiles();
             //null check
@@ -79,8 +85,7 @@ public class PersistenceDataService {
 
             //filer files to saving data
             for(File f : files){
-                if(f.getName().contains(SAVEFILE_NAME))
-                    savings.add(f.getName());
+                savings.add(f.getName());
             }
             return savings;
         }
@@ -95,9 +100,14 @@ public class PersistenceDataService {
      * @param savingName saving data name
      * return saved game data*/
     public static SavedGame loadGameData(String savingName) {
+        String path = System.getProperty("user.home")+File.separator;
+        String fileName = savingName;
         try{
+            if(!savingName.contains(FILE_EXT)){
+                fileName+=FILE_EXT;
+            }
             //load all file in default directory
-            File saveFile = new File(FILES_BASEPATH + SAVEFILE_PATH + savingName+FILE_EXT);
+            File saveFile = new File(path+FILES_BASEPATH + SAVEFILE_PATH + fileName);
             Scanner reader = new Scanner(saveFile);
             String jsonData = reader.nextLine();
             reader.close();
@@ -120,20 +130,25 @@ public class PersistenceDataService {
         ArrayList<String> configs = new ArrayList<String> (0);
 
         try{
-            //load all file in default directory
-            File dirFile = new File(FILES_BASEPATH+CONFIGFILE_PATH);
-            //load all files in default directory
-            File[] files = dirFile.listFiles();
-            //null check
-            if(files==null)
-                files = new File[0];
+            String path = PersistenceDataService.class.getResource(CONFIGFILE_PATH+LISTFILE_NAME+FILE_EXT).getFile();
+            File file  = new File(path);
+            Scanner reader = new Scanner(file);
+            String content = "";
 
-            //filer files to saving data
-            for(File f : files){
-                if(f.getName().contains(CONFIGFILE_NAME))
-                    configs.add(f.getName());
+            while (reader.hasNextLine()){
+                content+=reader.nextLine();
             }
-            return configs;
+
+            reader.close();
+
+            JSONObject jsonObject = new JSONObject(content);
+            JSONArray jsonArray = jsonObject.getJSONArray("levels");
+
+            for(int i = 0; i < jsonArray.length(); i++){
+                configs.add(jsonArray.get(i).toString());
+            }
+
+            return  configs;
         }
         catch (Exception e)
         {
@@ -146,16 +161,24 @@ public class PersistenceDataService {
      * @param  configName name of configuration to load
      * @return game configuration**/
     public static BeginningConfiguration loadConfig(String configName){
+        String fileName = configName;
         try{
+            if(!configName.contains(FILE_EXT)){
+                fileName+=FILE_EXT;
+            }
             //load all file in default directory
-            //String filePath = KlotskiApp.class.getResource("/GameData/"+configName+FILE_EXT).getFile();
-            //File configFile = new File(filePath);
-            File configFile = new File("D:/Java/klotski_project/src/main/resources/com/klotski/klotski_project/GameData/level1.json");
-            Scanner reader = new Scanner(configFile);
-            String jsonData = reader.nextLine();
+            File file  = new File(PersistenceDataService.class.getResource(CONFIGFILE_PATH+fileName).getFile());
+            Scanner reader = new Scanner(file);
+            String content = "";
+
+            while (reader.hasNextLine()){
+                content+=reader.nextLine();
+            }
             reader.close();
+
+
             //Load JSONObject
-            JSONObject configJSON = new JSONObject(jsonData);
+            JSONObject configJSON = new JSONObject(content);
             BeginningConfiguration config = BeginningConfiguration.fromJSON(configJSON);
             return config;
         }
@@ -172,12 +195,17 @@ public class PersistenceDataService {
     public static LevelSolution loadSolution(BeginningConfiguration beginConf) {
         try{
             //load all file in default directory
-            File configFile = new File(FILES_BASEPATH + SOLFILE_PATH+ beginConf.getName());
-            Scanner reader = new Scanner(configFile);
-            String jsonData = reader.nextLine();
+            File file  = new File(PersistenceDataService.class.getResource(SOLFILE_PATH+beginConf.getName()+FILE_EXT).getFile());
+            Scanner reader = new Scanner(file);
+            String content = "";
+
+            while (reader.hasNextLine()){
+                content+=reader.nextLine();
+            }
             reader.close();
+
             //Load JSONObject
-            JSONObject solutionJSON = new JSONObject(jsonData);
+            JSONObject solutionJSON = new JSONObject(content);
             LevelSolution solution  = LevelSolution.fromJSON(solutionJSON);
             return solution;
         }
---
+++ klotski_project/src/test/java/com/klotski/model/PersistenceDataServiceTest.java
@@ -0,0 +1,99 @@
+package com.klotski.model;
+
+import javafx.geometry.Point2D;
+import javafx.scene.paint.Color;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PersistenceDataServiceTest {
+
+    SavedGame savedGame;
+    Board board;
+
+    BeginningConfiguration config;
+
+    GameProgress gameProgress;
+    @BeforeEach
+    void setUp() {
+        ArrayList<Block> blocks= new ArrayList<Block>(0);
+
+        blocks.add(new Block(new Point2D(0,0), 1,1, Color.ORANGE));
+        blocks.add(new Block(new Point2D(0,1), 1,1, Color.ORANGE));
+
+        blocks.add(new Block(new Point2D(3,0), 1,1, Color.ORANGE));
+        blocks.add(new Block(new Point2D(3,1), 1,1, Color.ORANGE));
+
+        blocks.add(new Block(new Point2D(0,2), 2,1, Color.ORANGE));
+        blocks.add(new Block(new Point2D(3,2), 2,1, Color.ORANGE));
+
+        blocks.add(new Block(new Point2D(1,2), 1,1, Color.ORANGE));
+        blocks.add(new Block(new Point2D(1,3), 1,1, Color.ORANGE));
+
+        blocks.add(new Block(new Point2D(2,2), 1,1, Color.ORANGE));
+        blocks.add(new Block(new Point2D(2,3), 1,1, Color.ORANGE));
+
+        blocks.add(new Block(new Point2D(0,4), 1,1, Color.ORANGE));
+        blocks.add(new Block(new Point2D(3,4), 1,1, Color.ORANGE));
+
+
+        blocks.add(new Block(new Point2D(1,0), 2,2, Color.BLACK));
+
+        board= new Board(5,4,blocks);
+        config = new BeginningConfiguration("level1",blocks);
+        gameProgress = new GameProgress(config);
+        savedGame = new SavedGame(board,gameProgress);
+    }
+
+    @Test
+    void saveGameData() {
+        if(PersistenceDataService.saveGameData(savedGame)){
+            DateTimeFormatter dtf = DateTimeFormatter.ofPattern(PersistenceDataService.DATE_FORMAT);
+            String fileName = System.getProperty("user.home")+PersistenceDataService.SAVEFILE_PATH+PersistenceDataService.SAVEFILE_NAME +dtf.format(savedGame.getSaveDate())+PersistenceDataService.FILE_EXT;
+            assertFalse(Files.exists(Path.of(fileName)));
+        }
+    }
+
+    @Test
+    void loadAllGameData() {
+        List<String> files = PersistenceDataService.loadAllGameData();
+        assertFalse(files.isEmpty());
+    }
+
+    @Test
+    void loadGameData() {
+        String fileName = (PersistenceDataService.loadAllGameData()).get(0);
+        assertNotNull(PersistenceDataService.loadGameData(fileName));
+    }
+
+    @Test
+    void loadAllConfigurations() {
+        List<String> files = PersistenceDataService.loadAllConfigurations();
+        assertFalse(files.isEmpty());
+    }
+
+    @Test
+    void loadConfig() {
+        List<String> files = PersistenceDataService.loadAllConfigurations();
+        for(String file: files){
+            assertNotNull(PersistenceDataService.loadConfig(file));
+        }
+    }
+
+    @Test
+    void loadSolution() {
+        List<String> files = PersistenceDataService.loadAllConfigurations();
+        for(String file: files){
+            BeginningConfiguration config = PersistenceDataService.loadConfig(file);
+            assertNotNull(PersistenceDataService.loadSolution(config));
+        }
+    }
+}
